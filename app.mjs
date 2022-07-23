859d8cabc3c665a5beeaee927dd478c7f2af6a43
Oluwatobiloba Ibuola <ibuolatobi@gmail.com>
2022-07-23 12:00:02 +0100

    fix menubtn

--- app.mjs
+++ app.mjs
@@ -1,17 +1,41 @@
+let showMenu = false;
+const hamburger = document.querySelector('.header__bar--burger');
+const nav = document.querySelector('.header__nav');
+const menuNav = document.querySelector('.header__listContainer');
+const navItems = document.querySelectorAll('.header__listItem');
+
 function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
-  
-    // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
-  
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
-    }
-  };
-  
+  // Your entire app should not necessarily be coded inside this 
+  // single function (though there's no penalty for that), 
+  // so create and use/call additional functions from here
+
+  // pls remove the below and make some magic in here!
+  console.log('make magic in here!');
+
+  const header = document.querySelector('h2');
+  if(header) {
+    header.textContent = 'make some magic here!!';
+  }
+};
+ 
+
+function toggleMenu () {
+  if(!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach(item => item.classList.add('open'));
+
+    return showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach(item => item.classList.remove('open'));
+
+    return showMenu = false;
+  }
+}
   // ======= DO NOT EDIT ============== //
-  export default startApp;
+  export {startApp, toggleMenu};
   // ======= EEND DO NOT EDIT ========= //
