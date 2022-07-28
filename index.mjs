fa86a23b02de4248e982b8df3de002ae96b2bbe3
unknown <ibuolatobi@gmail.com>
2022-07-28 01:43:21 +0100

    refactor code

--- index.mjs
+++ index.mjs
@@ -1,33 +1,8 @@
-import {startApp , toggleMenu, toggleResult, selectMenu} from './app.mjs';
+import startApp from './app.mjs';
 
 document.addEventListener('DOMContentLoaded', startApp); 
 
 
-// showmenu dom manipulation
-const menuBtn = document.querySelector('.header__bar');
-menuBtn.addEventListener('click', function(){
-    console.log("clicked");
-    toggleMenu()
-});
-
-//toggleresult dom manipulation
-const resultBtns = document.querySelectorAll('.resultbtn');
-resultBtns.forEach(resultBtn => {
-  resultBtn.addEventListener('click', function(e){
-    console.log('result page clicked');
-    toggleResult(e);
-  })
-})
-
-// selectmenu dom manipulation
-const listEl =document.querySelectorAll('.header__link')
-listEl.forEach(list => {
-  list.addEventListener('click', (e) => {
-    console.log('clicked to select')
-    console.log(e)
-    selectMenu(e);
-  })
-})
 
 
 
